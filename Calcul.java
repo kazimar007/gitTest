fbb2b911c2c493a149c73d16d29dae89ac09a13a
kazimar007 <kazimar.terraria@gmail.com>
2021-02-01 17:49:28 -0500

    ajout methode soustraction

--- Calcul.java
+++ Calcul.java
@@ -2,4 +2,8 @@ public class Calcul {
     public static int addition(int n1, int n2){
         return n1 + n2;
     }
+
+    public static int soustraction(int n1, int n2){
+        return n1 - n2;
+    }
 }
